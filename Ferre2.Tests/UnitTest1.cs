34db11ae1cc47e3f32611670f02ea225fcc306cd
juandavidlis0223 <juandavidlis0223@gmail.com>
2026-02-08 13:53:02 -0500

    jñl

---
+++ Ferre2.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Ferre2.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void PruebaBasica()
+        {
+            Assert.True(true);
+        }
+    }
+}
